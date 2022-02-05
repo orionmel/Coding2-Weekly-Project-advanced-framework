07965237900d5e7b9144b828ebf7b394346178bd
Kexin Mei <64740521+orionmel@users.noreply.github.com>
2022-02-05 14:04:07 +0000

    Add files via upload

---
+++ week1_home❤️/some_objects.hpp
@@ -0,0 +1,25 @@
+//
+//  some_objects.hpp
+//  First_project
+//
+//  Created by mel on 2022/1/15.
+//
+
+#ifndef some_objects_hpp
+#define some_objects_hpp
+
+#include <stdio.h>
+#include<math.h>
+
+///------------------class---------------------------- ///
+class myClass{
+    int x = 100;
+    float y =3.14159;
+    
+public:
+    int c = 128;
+    float myFunc(float x ,float y);
+    
+};
+
+#endif /* some_objects_hpp */
---
+++ week1_home❤️/week1_home❤️/main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  First_project
+//
+//  Created by mel on 2022/1/15.
+//
+
+#include <iostream>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+#include "some_objects.hpp"
+
+
+using namespace std;
+
+//int myInt = 2.0;
+//float myP = 3.14159;
+//string myString = "Hey there";
+//
+//myClass bubbins1;
+//myClass bubbins2;
+
+
+int main(int argc, const char * argv[]) {
+
+//    float anotherFloat = bubbins1.myFunc(3,10)*bubbins2.myFunc(3,10);
+//    cout<< anotherFloat<<endl;
+  //  -------------
+    int i;
+    for(i=1;i<=10;i++)
+    {
+    cout<<" "<<i;
+    cout<<endl;//只要有这个计数的判断就好了，够多少就换行
+    }
+    return 0;
+    
+        
+}
